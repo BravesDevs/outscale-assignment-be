0f5291a93ed72b64cc277f5d5122dee59c793923
Alexei <devangparekh2014@gmail.com>
2024-01-05 16:28:44 -0500

    Book publish API

--- src/api/books.ts
+++ src/api/books.ts
@@ -1,7 +1,7 @@
 import { publishBookService } from "../services";
 export const publishBookController = async (req, res, next) => {
   try {
-    const response = await publishBookService(req.body);
+    return res.status(200).send(await publishBookService(req.body));
   } catch (err) {
     next(err);
   }
